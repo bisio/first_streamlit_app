0f31368447524af06e53584e0bd4d3761f332495
Andrea Bisognin <andrea.bisognin@scaiitec.it>
2022-11-28 17:17:43 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ try:
         streamlit.error('Please select a fruit to get information')
     else:
     # write your own comment -what does the next line do? 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # write your own comment - what does this do?
         streamlit.dataframe(fruityvice_normalized)
